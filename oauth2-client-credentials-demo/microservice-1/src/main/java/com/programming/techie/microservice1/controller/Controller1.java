5d1705542c680ac644e834c481df982dd9abbace
onepseudoxy <elidrissi.amine1@gmail.com>
2023-07-31 12:31:15 +0100

    Token relay implemented using Rest template and web client

--- oauth2-client-credentials-demo/microservice-1/src/main/java/com/programming/techie/microservice1/controller/Controller1.java
+++ oauth2-client-credentials-demo/microservice-1/src/main/java/com/programming/techie/microservice1/controller/Controller1.java
@@ -8,16 +8,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @RestController
 public class Controller1 {
 
-//    private final RestTemplate restTemplate= new RestTemplateBuilder().build();
+    private final RestTemplate restTemplate= new RestTemplateBuilder().build();
+    private final WebClient webClient= WebClient.builder().build();
+
 
     @GetMapping("/microservice1/home")
     @ResponseStatus(HttpStatus.OK)
     public String HelloRestTemplate(){
-        /*Jwt jwt = (Jwt) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        Jwt jwt = (Jwt) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 
         HttpHeaders httpHeaders= new HttpHeaders();
         httpHeaders.add("Authorization","Bearer "+jwt.getTokenValue());
@@ -27,7 +30,21 @@ public class Controller1 {
                 new HttpEntity<>(httpHeaders),
                 String.class);
 
-        return "PUY- Message from Microservice 2 is :"+ response.getBody();*/
-        return "Hello";
+        return "PUY- Message from Microservice 2 is :"+ response.getBody();
+    }
+
+    @GetMapping("microservice1/home/webclient")
+    @ResponseStatus(HttpStatus.OK)
+    public String helloWebClient(){
+        Jwt jwt = (Jwt) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        String response = webClient.get()
+                .uri("http://localhost:8084/microservice2/home")
+                .headers(header -> header.setBearerAuth(jwt.getTokenValue()))
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+
+        return "PUY- Message from Microservice 2 is :"+ response;
+
     }
 }
